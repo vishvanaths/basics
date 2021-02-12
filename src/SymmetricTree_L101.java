710e8437262ec6ff28249db3e3fdf46232b78e8d
vsconnected <vsconnected@gmail.com>
2021-02-12 00:05:40 -0500

    name correction

--- src/SymmetricTree_L101.java
+++ src/SymmetricTree_L101.java
@@ -6,15 +6,15 @@ public class SymmetricTree_L101 {
         if(root == null){
             return true;
         }
-        return isSymetricSubTree(root, root);
+        return isSymmetricSubTree(root, root);
     }
 
-    private boolean isSymetricSubTree(TreeNode n1, TreeNode n2) {
+    private boolean isSymmetricSubTree(TreeNode n1, TreeNode n2) {
         if(n1 == null && n2 == null) return true;
         if(n1 == null || n2 == null) return false;
 
         return (n1.val == n2.val)
-                && isSymetricSubTree(n1.left, n2.right)
-                && isSymetricSubTree(n1.right, n2.left);
+                && isSymmetricSubTree(n1.left, n2.right)
+                && isSymmetricSubTree(n1.right, n2.left);
     }
 }
